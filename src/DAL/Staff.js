7602eae8734d2399d1c5f14e7a557d8f4052af51
Vitalii Rezukhin <boshka.2012@yandex.ru>
2021-07-31 14:07:12 +0500

    new changes with DAL and ReduxStore

---
+++ src/DAL/Staff.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const URL_STAFF = 'http://localhost:3000/users';
+
+const URL_EMPLOYEE = (employee) => {
+    return URL_STAFF + `/${employee.id}/`;
+}
+
+const DAL_STAFF = {
+    Get: GetStaff,
+    Add: AddEmployee,
+    Delete: DeleteEmployee,
+    Update: UpdateEmployee
+};
+
+const GetStaff = () => {
+    return  axios.get(URL_STAFF).then(resp => resp.data)
+        .catch(() => null);
+}
+
+const AddEmployee = (employee) => {
+    return axios.post(URL_STAFF, employee).then(resp => resp.data)
+        .catch(() => null);
+}
+
+const DeleteEmployee = (employee) => {
+    return axios.delete(URL_EMPLOYEE(employee)).then(resp => resp.data)
+        .catch(() => null);
+}
+
+const UpdateEmployee = (employee) => {
+    return axios.put(URL_EMPLOYEE(employee), employee).then(resp => resp.data)
+        .catch(() => null);
+}
+
+export default DAL_STAFF;
--- src/store/reducers/staffReducer.js
+++ src/store/reducers/staffReducer.js
@@ -1,10 +1,32 @@
-const initialState = {}
+const GET_STAFF = "GET_STAFF";
+const ADD_EMPLOYEE = "ADD_EMPLOYEE";
+const DELETE_EMPLOYEE = "DELETE_EMPLOYEE";
+const UPDATE_EMPLOYEE = "UPDATE_EMPLOYEE";
+
+
+const initialState = {
+    staff: []
+}
 
 const staffReducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_STAFF:
+            return {...state, staff: action.staff};
+        case ADD_EMPLOYEE:
+            return {...state, staff: [...state.staff, action.employee]};
+        case DELETE_EMPLOYEE:
+            return {...state, staff: state.staff.filter(employee => employee.id !== action.employee.id)};
+        case UPDATE_EMPLOYEE:
+            return {...state, staff: state.staff.map(employee => employee.id === action.employee.id ? action.employee : employee)};
         default:
             return state;
     }
 }
+
+export const GetStaffAC = (staff) => {return {type: GET_STAFF, staff}};
+export const AddEmployeeAC = (employee) => {return {type: ADD_EMPLOYEE, employee}};
+export const DeleteEmployeeAC = (employee) => {return {type: DELETE_EMPLOYEE, employee}};
+export const UpdateEmployeeAC = (employee) => {return {type: UPDATE_EMPLOYEE, employee}};
+
 
 export default staffReducer;
